9741952d4c2b42a0b3dee9ffe5261272e7119567
AndreyPerunov <104921155+AndreyPerunov@users.noreply.github.com>
2023-11-30 23:52:11 +0200

    Added custom placeholder for the search

--- src/app/Search.tsx
+++ src/app/Search.tsx
@@ -1,12 +1,18 @@
 "use client"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 export default function Search() {
+  const possibleSearches = ["ice cream", "sweet bananas", "grilling", "baking", "fresh", "meat", "seafood", "fruits", "bakery", "dairy", "vegetables", "sweets", "pasta", "snacks", "crunchy", "vanilla ice cream", "juicy sausages", "probiotic-rich yogurt", "grain bread", "breakfast essential"]
+  const [placeholder, setPlaceholder] = useState<string>("...")
   const [search, setSearch] = useState<string>("")
+
+  useEffect(() => {
+    setPlaceholder(possibleSearches[Math.floor(Math.random() * possibleSearches.length)])
+  }, [])
 
   return (
     <search className="bg-white dark:bg-slate-800 dark:border-slate-700 dark:text-slate-100 p-1 rounded-2xl border-2 w-full max-w-lg flex focus-within:border-purple-500 shadow-lg">
-      <input value={search} onChange={e => setSearch(e.target.value)} className="p-2 bg-transparent flex-1 focus:outline-none" type="text" placeholder="search" />
+      <input value={search} onChange={e => setSearch(e.target.value)} className="p-2 bg-transparent flex-1 focus:outline-none" type="text" placeholder={placeholder} />
     </search>
   )
 }
