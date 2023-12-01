9f9fdf6b563a5c525e3191f582e7dd3809b32d32
AndreyPerunov <104921155+AndreyPerunov@users.noreply.github.com>
2023-12-01 17:25:39 +0200

    Debounced Search

--- src/app/Search.tsx
+++ src/app/Search.tsx
@@ -1,14 +1,36 @@
 "use client"
 import { useEffect, useState } from "react"
+import { useRouter } from "next/navigation"
 
 export default function Search() {
-  const possibleSearches = ["ice cream", "sweet bananas", "grilling", "baking", "fresh", "meat", "seafood", "fruits", "bakery", "dairy", "vegetables", "sweets", "pasta", "snacks", "crunchy", "vanilla ice cream", "juicy sausages", "probiotic-rich yogurt", "grain bread", "breakfast essential"]
   const [placeholder, setPlaceholder] = useState<string>("...")
   const [search, setSearch] = useState<string>("")
+  const [debouncedSearch, setDebouncedSearch] = useState<string>("")
+  const router = useRouter()
 
   useEffect(() => {
+    const possibleSearches = ["ice cream", "sweet bananas", "grilling", "baking", "fresh", "meat", "seafood", "fruits", "bakery", "dairy", "vegetables", "sweets", "pasta", "snacks", "crunchy", "vanilla ice cream", "juicy sausages", "probiotic-rich yogurt", "grain bread", "breakfast essential"]
+    // Choose a random placeholder
     setPlaceholder(possibleSearches[Math.floor(Math.random() * possibleSearches.length)])
   }, [])
+
+  // Debouncing search
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setDebouncedSearch(search)
+    }, 300)
+
+    return () => clearTimeout(timeout)
+  }, [search])
+
+  // Update the URL with the search query
+  useEffect(() => {
+    if (debouncedSearch) {
+      router.push(`/?search=${debouncedSearch}`)
+    } else {
+      router.push("/")
+    }
+  }, [debouncedSearch, router])
 
   return (
     <search className="bg-white dark:bg-slate-800 dark:border-slate-700 dark:text-slate-100 p-1 rounded-2xl border-2 w-full max-w-lg flex focus-within:border-purple-500 shadow-lg">
