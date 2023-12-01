ec5c83fc49a2ed2758413fda5092adbef3d6c962
AndreyPerunov <104921155+AndreyPerunov@users.noreply.github.com>
2023-12-01 22:17:05 +0200

    Added pagination

--- src/app/Navigation.tsx
+++ src/app/Navigation.tsx
@@ -1,15 +1,43 @@
-const Navigation: React.FC = () => {
+import Link from "next/link"
+
+export default function Navigation({ page, search, pagesNumber }: { page: number; search: string; pagesNumber: number }) {
   return (
-    <nav className="my-10">
-      <button className="">1</button>
-      <button className="">2</button>
-      <button disabled className="">
-        ...
-      </button>
-      <button className="">99</button>
-      <button className="">100</button>
+    <nav className="my-5 w-full flex justify-center items-center">
+      {1 < page ? (
+        <Link
+          className="rounded-lg bg-white dark:bg-slate-800 dark:text-slate-500 p-2 px-4 shadow-lg border-2 border-transparent hover:text-purple-700 dark:hover:text-purple-500 hover:border-purple-700 dark:hover:border-purple-500"
+          href={{
+            pathname: "/",
+            query: {
+              ...(search ? { search } : {}),
+              page: page - 1
+            }
+          }}
+        >
+          {"<"}
+        </Link>
+      ) : (
+        <span className="rounded-lg bg-white dark:bg-slate-800 dark:text-slate-700 p-2 px-4 shadow-lg border-2 border-transparent text-gray-400 cursor-not-allowed">{"<"}</span>
+      )}
+      <span className="mx-5 text-center dark:text-slate-500">
+        {page} / {pagesNumber}
+      </span>
+      {pagesNumber > page ? (
+        <Link
+          className="rounded-lg bg-white dark:bg-slate-800 dark:text-slate-500 p-2 px-4 shadow-lg border-2 border-transparent hover:text-purple-700 dark:hover:text-purple-500 hover:border-purple-700 dark:hover:border-purple-500"
+          href={{
+            pathname: "/",
+            query: {
+              ...(search ? { search } : {}),
+              page: page + 1
+            }
+          }}
+        >
+          {">"}
+        </Link>
+      ) : (
+        <span className="rounded-lg bg-white dark:bg-slate-800 dark:text-slate-700 p-2 px-4 shadow-lg border-2 border-transparent text-gray-400 cursor-not-allowed">{">"}</span>
+      )}
     </nav>
   )
 }
-
-export default Navigation
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,8 +17,21 @@ async function getProducts() {
 }
 
 export default async function Home({ searchParams }: { searchParams?: { [key: string]: string | undefined } }) {
-  const products: Product[] | undefined = await getProducts()
   const search = searchParams?.search || ""
+  let page: number = Number(searchParams?.page) || 1
+  let limit: number = Number(searchParams?.limit) || 12
+
+  const products: Product[] | undefined = await getProducts()
+  const productsFiltered: Product[] = products.filter(product => {
+    if (!search) return true
+    if (search === "") return true
+    if (product.name.toLowerCase().includes(search.toLowerCase())) return true
+    if (product.category.toLowerCase().includes(search.toLowerCase())) return true
+    if (product.description.toLowerCase().includes(search.toLowerCase())) return true
+  })
+  const productsPaginated: Product[] = productsFiltered.slice((page - 1) * limit, page * limit)
+
+  const pagesNumber: number = Math.ceil(productsFiltered.length / limit)
 
   return (
     <>
@@ -28,29 +41,20 @@ export default async function Home({ searchParams }: { searchParams?: { [key: st
       <Search initialSearch={search} />
 
       <section className="place-items-stretch w-full grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 mt-12">
-        {products
-          .filter(product => {
-            if (!search) return true
-            if (search === "") return true
-            if (product.name.toLowerCase().includes(search.toLowerCase())) return true
-            if (product.category.toLowerCase().includes(search.toLowerCase())) return true
-            if (product.description.toLowerCase().includes(search.toLowerCase())) return true
-          })
-          .map(product => (
-            <a key={product.id} href={"/product/" + product.id}>
-              <article className="h-full min-h-[14rem] rounded-lg bg-white dark:bg-slate-800 p-5 shadow-lg flex flex-col border-2 border-transparent hover:scale-110 transition hover:border-purple-700 dark:hover:border-purple-500">
-                <p className="font-medium text-gray-400 dark:text-slate-500">{product.category}</p>
-                <h2 className="font-bold text-xl mb-4 dark:text-slate-100">{product.name}</h2>
-                <p className="flex-1 text-sm mb-4 dark:text-slate-400">{product.description}</p>
-                <p className="text-xl text-right font-semibold text-transparent bg-clip-text bg-gradient-to-tr from-purple-700 to-pink-900 dark:from-purple-400 dark:to-pink-400">
-                  {product.price} {product.currency}
-                </p>
-              </article>
-            </a>
-          ))}
+        {productsPaginated.map(product => (
+          <a key={product.id} href={"/product/" + product.id}>
+            <article className="h-full min-h-[14rem] rounded-lg bg-white dark:bg-slate-800 p-5 shadow-lg flex flex-col border-2 border-transparent hover:scale-110 transition hover:border-purple-700 dark:hover:border-purple-500">
+              <p className="font-medium text-gray-400 dark:text-slate-500">{product.category}</p>
+              <h2 className="font-bold text-xl mb-4 dark:text-slate-100">{product.name}</h2>
+              <p className="flex-1 text-sm mb-4 dark:text-slate-400">{product.description}</p>
+              <p className="text-xl text-right font-semibold text-transparent bg-clip-text bg-gradient-to-tr from-purple-700 to-pink-900 dark:from-purple-400 dark:to-pink-400">
+                {product.price} {product.currency}
+              </p>
+            </article>
+          </a>
+        ))}
       </section>
-
-      <Navigation />
+      <Navigation page={page} pagesNumber={pagesNumber} search={search} />
     </>
   )
 }
